503e34d71fa981052bfde70f1bd6f0445ed6c0a3
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-06-28 15:48:02 +0300

    27/06: - Finished A.1. - Finished A.2.

--- src/LevelLargestSum.java
+++ src/LevelLargestSum.java
@@ -1,17 +1,49 @@
+import java.util.ArrayDeque;
+
 public class LevelLargestSum {
     public static int getLevelWithLargestSum(BinNode<Integer> root) {
         // TODO: Add your code for part A2 here...
-        if (root.getData() == null) {
+        if (root == null) {
             return -1;
         }
 
-        if (root.getLeft() == null && root.getRight() == null) {
-            return 0;
+        int maxLevel = 0;
+        int maxSum = root.getData();
+        int currentLevel = 0;
+        int currentSum = 0;
+
+        ArrayDeque<BinNode<Integer>> queue = new ArrayDeque<>();
+        queue.offer(root);
+        queue.offer(null);
+
+        while (!queue.isEmpty()) {
+            BinNode<Integer> node = queue.poll();
+
+            if (node == null) {
+                if (!queue.isEmpty()) {
+                    queue.offer(null);
+                }
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    maxLevel = currentLevel;
+                }
+
+                currentSum = 0;
+                currentLevel++;
+            } else {
+                currentSum += node.getData();
+
+                if (node.getLeft() != null) {
+                    queue.offer(node.getLeft());
+                }
+
+                if (node.getRight() != null) {
+                    queue.offer(node.getRight());
+                }
+            }
         }
 
-        if ((root.getLeft().getData() + root.getRight().getData() > root.getData())) {
-            return (getLevelWithLargestSum(root.getLeft()) + getLevelWithLargestSum(root.getRight()));
-        }
-        return 1;
+        return maxLevel;
     }
 }
