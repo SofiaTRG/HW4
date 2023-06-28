34178578510967dcef005af121aa15f7787a111d
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-06-28 10:58:45 +0300

    27/06: - Finished A.1.

--- src/LevelLargestSum.java
+++ src/LevelLargestSum.java
@@ -1,5 +1,17 @@
 public class LevelLargestSum {
     public static int getLevelWithLargestSum(BinNode<Integer> root) {
         // TODO: Add your code for part A2 here...
+        if (root.getData() == null) {
+            return -1;
+        }
+
+        if (root.getLeft() == null && root.getRight() == null) {
+            return 0;
+        }
+
+        if ((root.getLeft().getData() + root.getRight().getData() > root.getData())) {
+            return (getLevelWithLargestSum(root.getLeft()) + getLevelWithLargestSum(root.getRight()));
+        }
+        return 1;
     }
 }
