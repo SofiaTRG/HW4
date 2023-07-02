0b20529410e0a9983bb754cd96a39500e96d1368
Sofia Torgovezky <sofiat@campus.technion.ac.il>
2023-07-02 16:12:37 +0300

    27/06: - Finished A.1. - Finished A.2.

--- src/Database.java
+++ src/Database.java
@@ -1,160 +1,103 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
 public class Database {
-    private static int maxRead;
-    private static ThreadLocal<Boolean> workHere = new ThreadLocal<>();
-    private static Lock lockRead=new ReentrantLock();
-    private static Lock lockWrite=new ReentrantLock();
-    private static int numOfRead;
-
-
     private Map<String, String> data;
+    private static int maxRead;
+    private static int numCurrentReading;
+    private static Lock lockRead = new ReentrantLock();
+    private static Lock lockWrite = new ReentrantLock();
+    private static Set<Thread> nowReadWork = new HashSet<>();
 
     public Database(int maxNumOfReaders) {
-        data = new HashMap<>();  // Note: You may add fields to the class and initialize them in here. Do not add parameters!
-        maxRead=maxNumOfReaders;
-        workHere.set(false);
-        numOfRead=0;
+        data = new HashMap<>();
+        maxRead = maxNumOfReaders;
+        numCurrentReading = 0;
     }
 
-    /**
-     * putinig value into the date bace
-     * @param key where to put
-     * @param value what to put
-     */
     public void put(String key, String value) {
-        //writeTryAcquire();
         data.put(key, value);
-        //writeRelease();
     }
 
-    /**
-     *
-     * @param key from where take
-     * @return returning value from the date base
-     */
     public String get(String key) {
-        //readTryAcquire();
-        //String help=data.get(key);
-        //readRelease();
         return data.get(key);
-
     }
 
-    /**
-     *
-     * @return true if get green light to start read and false if not
-     */
     public boolean readTryAcquire() {
-        // TODO: Add your code here...
-        if(lockWrite.tryLock()) //check no one writing
-        {
-            if(lockRead.tryLock())//no one read
-                if(numOfRead<maxRead)//if he can start read
-                {
-                    numOfRead++;//adding to the count of people reading
-                    lockRead.unlock();//unlock all locks
-                    workHere.set(true);//mark that this proses started
+        if (lockWrite.tryLock()) {
+            if (lockRead.tryLock()) {
+                if (numCurrentReading < maxRead) {
+                    numCurrentReading++;
+                    lockRead.unlock();
+                    nowReadWork.add(Thread.currentThread());
                     lockWrite.unlock();
                     return true;
                 }
-            lockRead.unlock();//unlock all
+                lockRead.unlock();
+            }
             lockWrite.unlock();
         }
         return false;
     }
 
-    /**
-     * waiting until getting premisses to start reading
-     */
     public void readAcquire() {
-        while (!lockWrite.tryLock()); //stop from writing
-        boolean acquire = false;
-        while (!acquire) {
-            lockRead.lock(); //stop others from reading
-            if (numOfRead < maxRead) {//check if he can start
-                numOfRead++;
-                acquire = true;
+        boolean flag = false;
+        while (!lockWrite.tryLock());
+        while (!flag) {
+            lockRead.lock();
+            if (numCurrentReading < maxRead) {
+                numCurrentReading++;
+                flag = true;
             }
-            lockRead.unlock();//unlock read
+            lockRead.unlock();
         }
-        lockWrite.unlock();//unlock write
-        workHere.set(true);//mark that this proses started
-        // TODO: Add your code here...
+        lockWrite.unlock();
+        nowReadWork.add(Thread.currentThread());
     }
 
-    /**
-     * release reading
-     * @throws IllegalMonitorStateException if calles but procese dont started
-     */
-    public void readRelease()throws IllegalMonitorStateException {
-        //threi exeption
-        if(!workHere.get()){
-            String s="Illegal write release attempt";
-            try{
-                throw new IllegalMonitorStateException(s);
-            }catch (IllegalMonitorStateException e){
-                throw new IllegalMonitorStateException(s);
-            }
-        }
-        else {
-            // TODO: Add your code here...
-
-            numOfRead = numOfRead - 1;
-            workHere.set(false);
+    public void readRelease() throws IllegalMonitorStateException {
+        if (!nowReadWork.contains(Thread.currentThread())) {
+            String errorMessage = "Illegal read release attempt";
+            throw new IllegalMonitorStateException(errorMessage);
+        } else {
+            numCurrentReading--;
+            nowReadWork.remove(Thread.currentThread());
         }
     }
 
-    /**
-     * geting green light to start writing
-     */
     public void writeAcquire() {
         while (!lockWrite.tryLock());
         while (!lockRead.tryLock());
-        while (numOfRead!=0);
-        workHere.set(true);
+        while (numCurrentReading != 0);
+        nowReadWork.add(Thread.currentThread());
     }
-    // TODO: Add your code here...
 
-    /**
-     *
-     * @return true if can start writing and false othewise
-     */
     public boolean writeTryAcquire() {
-        // TODO: Add your code here...
-        if(lockWrite.tryLock())
-            if(lockRead.tryLock())
-                if(numOfRead==0){
-                    workHere.set(true);
+        if (lockWrite.tryLock()) {
+            if (lockRead.tryLock()) {
+                if (numCurrentReading == 0) {
+                    nowReadWork.add(Thread.currentThread());
                     return true;
-
                 }
-
-
+                lockRead.unlock();
+            }
+            lockWrite.unlock();
+        }
         return false;
-
     }
 
-    /**
-     * relese write
-     * @throws IllegalMonitorStateException if calls but procese dont started
-     */
     public void writeRelease() throws IllegalMonitorStateException {
-        if(!workHere.get()){
-            String s="Illegal write release attempt";
-            try {
-                throw new IllegalMonitorStateException(s);
-            }catch (IllegalMonitorStateException e){
-                throw new IllegalMonitorStateException(s);
-            }
-        }
-        else {
+        if (!nowReadWork.contains(Thread.currentThread())) {
+            String errorMessage = "Illegal write release attempt";
+            throw new IllegalMonitorStateException(errorMessage);
+        } else {
             lockWrite.unlock();
             lockRead.unlock();
-            workHere.set(false);
+            nowReadWork.remove(Thread.currentThread());
         }
-        // TODO: Add your code here...
     }
 }
